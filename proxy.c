1a35290f15342f7d11e5b212e6234466390de88f
merosy07 <130292161+merosy07@users.noreply.github.com>
2024-09-05 13:17:55 +0800

    Update proxy.c

--- proxy.c
+++ proxy.c
@@ -8,14 +8,29 @@
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
+struct {
+    char url[MAXLINE], content[MAX_OBJECT_SIZE];
+    int cnt, Used; 
+} cache[10];
+
+static int readcnt; 
+static sem_t readcnt_mutex, writer_mutex;
+
 void *thread(void *);
 void doit(int client_fd);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 void parse_uri(char *uri, char *hostname, char *path, int *port);
 void build_new_request_hdr(rio_t *rio_packet, char *new_request, char *hostname, char *port);
 
-int main(int argc, char **argv) {
-    int listenfd, *connfd;
+int reader(int fd, char* url);
+void writer(int* buf, char* url);
+
+
+int main(int argc, char **argv) {   
+    Sem_init(&readcnt_mutex, 0, 1);
+    Sem_init(&writer_mutex, 0, 1);
+
+    int listenfd, *connfd_ptr;
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
@@ -30,17 +45,18 @@ int main(int argc, char **argv) {
     listenfd = Open_listenfd(argv[1]);
     while (1) {
 		clientlen = sizeof(clientaddr);
-        connfd = Malloc(sizeof(int));
-		*connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); 
+        connfd_ptr = Malloc(sizeof(int));
+		*connfd_ptr = Accept(listenfd, (SA *)&clientaddr, &clientlen); 
     	Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", hostname, port);
-//      doit(connfd); Close(connfd);  
-        Pthread_create(&tid, NULL, thread, connfd);                                                                       
+//      doit(connfd); Close(connfd);  	
+        Pthread_create(&tid, NULL, thread, connfd_ptr);                                                                                    
     }
+    return 0;
 }
 
 
-void *thread(void *ptr){
+void *thread(void *ptr) {
     int connfd = *((int *)ptr);
     Pthread_detach(pthread_self());
     doit(connfd);
@@ -51,23 +67,29 @@ void *thread(void *ptr){
 
 
 void doit(int client_fd) {
-    int real_server_fd, port, len;
-    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE], hostname[MAXLINE], path[MAXLINE];
+    int real_server_fd, port;
+    char buf[MAXLINE], method[MAXLINE], url[MAXLINE], version[MAXLINE], uri[MAXLINE], obj_buf[MAXLINE], hostname[MAXLINE], path[MAXLINE];
     rio_t real_client, real_server;
 
     Rio_readinitb(&real_client, client_fd);
     if (!Rio_readlineb(&real_client, buf, MAXLINE))  	 
         return;
-    sscanf(buf, "%s %s %s", method, uri, version);       
+    sscanf(buf, "%s %s %s", method, uri, version);
+    strcpy(url, uri);       
     if (strcasecmp(method, "GET")) {                     
         clienterror(client_fd, method, "501", "Not Implemented","Tiny does not implement this method");
         return;
-    }                                                    
-    
+    }
+
+    if(reader(client_fd, url)) {
+        fprintf(stdout, "%s from cache\n", url);
+        return;
+    }
+
     parse_uri(uri, hostname, path, &port);
-    char port_str[10];
-    sprintf(port_str, "%d", port); 
-    real_server_fd = Open_clientfd(hostname, port_str);  
+    char port_str[0];
+    sprintf(port_str, "%d", port);
+    real_server_fd = Open_clientfd(hostname, port_str);
 	if(real_server_fd < 0){
         printf("connection failed\n");
         return;
@@ -80,14 +102,26 @@ void doit(int client_fd) {
 
     Rio_writen(real_server_fd, new_request, strlen(new_request));
     
-    while((len = Rio_readlineb(&real_server, buf, MAXLINE)))
+    int obj_size = 0, len;
+    while((len = Rio_readlineb(&real_server, buf, MAXLINE))){
         Rio_writen(client_fd, buf, len);
+
+        if(obj_size + len < MAX_OBJECT_SIZE) {
+            strcpy(obj_buf + obj_size, buf);
+            obj_size += len;
+        }
+    }
+
+    if(obj_size < MAX_OBJECT_SIZE)
+        writer(obj_buf, url);
+
+    Close(real_server_fd);
 }
 
 
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg) {
     char buf[MAXLINE], body[MAXBUF];
-    
+
     /* Build the HTTP response body */
     sprintf(body, "<html><title>Tiny Error</title>");
     sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
@@ -136,11 +170,12 @@ void parse_uri(char *uri, char *hostname, char *path, int *port) {
 }
 
 
-void build_new_request_hdr(rio_t *real_client, char *new_request, char *hostname, char *port){
+void build_new_request_hdr(rio_t *real_client, char *new_request, char *hostname, char *port) {
     char temp_buf[MAXLINE];
 
     while(Rio_readlineb(real_client, temp_buf, MAXLINE) > 0){
         if (strstr(temp_buf, "\r\n")) break; 
+
         if (strstr(temp_buf, "Host:")) continue;
         if (strstr(temp_buf, "User-Agent:")) continue;
         if (strstr(temp_buf, "Connection:")) continue;
@@ -152,4 +187,62 @@ void build_new_request_hdr(rio_t *real_client, char *new_request, char *hostname
     sprintf(new_request, "%sHost: %s:%s\r\n", new_request, hostname, port);
     sprintf(new_request, "%s%s%s%s", new_request, user_agent_hdr, "Connection: close\r\n", "Proxy-Connection: close\r\n");
     sprintf(new_request,"%s\r\n", new_request);
+}
+
+
+
+
+int reader(int fd, char* url){
+    while(1){
+        int Cached = 0; 
+
+        P(&readcnt_mutex);
+        readcnt++;
+        if(readcnt == 1)
+            P(&writer_mutex);
+        V(&readcnt_mutex);
+
+        for(int i = 0; i < 10; i++){
+            if(cache[i].Used && (strcmp(url, cache[i].url) == 0)){
+                Cached = 1;
+                Rio_writen(fd, cache[i].content, MAX_OBJECT_SIZE);
+                cache[i].cnt++;
+                break;
+            }
+        }
+
+        P(&readcnt_mutex);
+        readcnt--;
+        if(readcnt == 0) 
+            V(&writer_mutex);
+        V(&readcnt_mutex);
+
+        return Cached;        
+    }
+}
+
+
+void writer(int* buf, char* url){
+    while(1){
+        int Least = cache[0].cnt, LRU_obj;
+
+        P(&writer_mutex);
+
+        for(int i = 0; i < 10; i++){
+            if(cache[i].Used == 0){
+                LRU_obj = i;
+                break;
+            }
+            if(cache[i].cnt < Least){ 
+                LRU_obj = i;
+                Least = cache[i].cnt;
+            }
+        }
+        strcpy(cache[LRU_obj].url, url);
+        strcpy(cache[LRU_obj].content, buf);
+        cache[LRU_obj].cnt = 0;
+        cache[LRU_obj].Used = 1;
+
+        V(&writer_mutex);
+    }
 }
